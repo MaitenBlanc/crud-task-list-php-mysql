2de3460b2debc1c0fda22d5fc8df5caa46f126fc
Your Name <you@example.com>
2024-04-01 15:02:33 -0300

    finished

---
+++ db.php
@@ -0,0 +1,10 @@
+<?php
+
+session_start();
+
+$conn = mysqli_connect(
+    'localhost',
+    'root',
+    'admin',
+    'php_crud_mysql'
+);
---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<!-- Scripts -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+<!-- Font Awesome 6.3.0 -->
+<script src="https://kit.fontawesome.com/68f56161a9.js" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
